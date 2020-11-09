b72cd0457b3261000208c6474f944231bf7bed39
Titouan Dessus <titouan.dessus@gmail.com>
2020-11-09 04:03:22 +0100

    initial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,14 @@
+class HomeController < ApplicationController
+  def index
+    render json: {
+      statut: 200, 
+      service: 'blog_api', 
+      version: '0.1.0', 
+      auteur: 'Titouax', 
+      description: 'Une api pour un blog',
+      routes: {
+        "root": "/", 
+        },
+    }, status: :ok
+  end
+end
